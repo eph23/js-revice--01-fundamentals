f4d78ec220923ad47d6975c30844120731cc35c3
ephraim <ephraims.818@gmail.com>
2025-09-10 21:40:21 +0600

    JS-REVICE--COMPLETE-JS---FUNDAMENTALS - 01. Function Declarations

---
+++ 01. Functions/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/* Function Declaration */
+function logger() {
+    console.log(`My name is Eph`);
+}
+
+/* Calling/invoking/running function */
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
