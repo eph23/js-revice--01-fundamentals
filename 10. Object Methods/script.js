b531f9364a81516961a512aecc8b9f9898e7cf72
ephraim <ephraims.818@gmail.com>
2025-09-13 14:26:19 +0600

    JS-REVICE--COMPLETE-JS---FUNDAMENTALS - 10. Object Methods

---
+++ 10. Object Methods/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const eph = {
+    firstName: "Eph",
+    lastName: "Sangma",
+    birthYear: 1988,
+    job: "Web Dev",
+    friends: ["Michael", "Steven", "Bob"],
+    hasDriversLicense: true,
+
+    calcAge: function () {
+        console.log(this);
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+
+    //     Challenge
+    getSummery: function () {
+        return `${this.firstName} is a ${this.calcAge()} years old ${
+            this.job
+        } and he has ${this.hasDriversLicense ? "a" : "no"} drivers license`;
+    },
+};
+
+console.log(eph.calcAge());
+console.log(eph.age);
+
+// Challenge
+/* Write a getSummery method to get summery about eph
+Eph is a 49 years old Web Dev and he has a drivers license
+*/
+console.log(eph.getSummery());
