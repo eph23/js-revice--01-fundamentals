f2d80f63580c26c2dad65b8945893c74bc9bd5e6
ephraim <ephraims.818@gmail.com>
2025-09-14 15:24:06 +0600

    JS-REVICE--COMPLETE-JS---FUNDAMENTALS - 11. Iteration The for Loop

---
+++ 11. Iteration The for Loop/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// the for loop
+for (let i = 1; i <= 5; i++) {
+    console.log(`Lifting weights! Rep ${i}`);
+}
+
+const word = "Hello World";
+for (let i = 0; i <= word.length - 1; i++) {
+    console.log(`${word[i]}`);
+}
